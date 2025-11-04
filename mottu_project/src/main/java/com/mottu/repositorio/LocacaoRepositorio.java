89acc758eb39b886b47149eddb9abbd0081bd5b4
leoc7sar <161585547+leoc7sar@users.noreply.github.com>
2025-11-04 11:14:08 -0300

    Delete mottu_project directory

--- mottu_project/src/main/java/com/mottu/repositorio/LocacaoRepositorio.java
+++
@@ -1,4 +0,0 @@
-package com.mottu.repositorio;
-import com.mottu.dominio.Locacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface LocacaoRepositorio extends JpaRepository<Locacao, Long> {}
--- mottu_project/src/main/java/com/mottu/repositorio/MotoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.mottu.repositorio;
-import com.mottu.dominio.Moto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface MotoRepositorio extends JpaRepository<Moto, Long> {
-  List<Moto> findByDisponivelTrue();
-}
